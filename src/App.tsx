da2426a51490f88f1a5505bf22638e7306a0175b
Edg3Runner <mitch.pauwels@proton.me>
2024-04-24 20:43:13 +0100

    Mobile Layout Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import myImg from "./assets/images/illustration-article.svg";
+import profilePicture from "./assets/images/image-avatar.webp";
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <main className="h-dvh bg-custom-yellow flex justify-center items-center">
+      <div className="w-80 bg-white flex flex-col gap-6 p-8 rounded-2xl">
+        <img className="rounded-lg" src={myImg} alt="image" />
+        <span className="text-black font-bold bg-custom-yellow px-4 py-1 self-start	">
+          learning
+        </span>
+        <p>Published 21 Dec 2023</p>
+        <h1 className="font-extrabold text-xl">HTML & CSS foundations</h1>
+        <p className="text-sm text-gray-500">
+          These languages are the backbone of every website, defining structure,
+          content, and presentation.
+        </p>
+        <div className="flex items-center gap-4">
+          <img src={profilePicture} alt="profile-picture" />
+          <h2 className="font-bold text-sm">Greg Hooper</h2>
+        </div>
+      </div>
+    </main>
   );
 }
 
