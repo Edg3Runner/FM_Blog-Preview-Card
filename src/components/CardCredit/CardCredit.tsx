b985ec2a0799ddbe03a05e74e5ced0ad6fd59354
Edg3Runner <mitch.pauwels@proton.me>
2024-04-25 14:40:37 +0100

    Adjusted width  credit picture

--- src/components/CardCredit/CardCredit.tsx
+++ src/components/CardCredit/CardCredit.tsx
@@ -3,7 +3,7 @@ import profilePicture from "../../assets/images/image-avatar.webp";
 export default function CardCredit() {
   return (
     <div className="flex items-center gap-4">
-      <img src={profilePicture} alt="profile-picture" />
+      <img className="w-8" src={profilePicture} alt="profile-picture" />
       <h2 className="font-bold text-sm">Greg Hooper</h2>
     </div>
   );
