819d85e03817688c86bb34b794e234b8770d7894
Edg3Runner <mitch.pauwels@proton.me>
2024-04-25 15:00:44 +0100

    Added border and boxShadow

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,7 +6,7 @@ import Cardlabel from "../CardLabel/Cardlabel";
 export default function Card() {
   return (
     <main className="h-dvh bg-custom-yellow flex justify-center items-center">
-      <div className="w-80 bg-white flex flex-col gap-6 p-8 rounded-2xl">
+      <div className="w-96 bg-white flex flex-col gap-6 p-8 rounded-2xl shadow-[8px_8px_0px_1px_rgba(0,0,0,0.3)] shadow-black border-1 border-black ">
         <CardImage />
         <Cardlabel />
         <CardContent />
