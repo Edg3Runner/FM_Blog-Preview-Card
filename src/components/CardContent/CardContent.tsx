afdca6857cab1dd26197b767f5161007400fd9b4
Edg3Runner <mitch.pauwels@proton.me>
2024-04-25 15:51:52 +0100

    Added hover styling, gap to content

--- src/components/CardContent/CardContent.tsx
+++ src/components/CardContent/CardContent.tsx
@@ -1,12 +1,14 @@
 export default function CardContent() {
   return (
-    <>
+    <div className="flex flex-col gap-3">
       <p className="font-medium text-sm">Published 21 Dec 2023</p>
-      <h1 className="font-extrabold text-xl">HTML & CSS foundations</h1>
+      <a className="hover:text-custom-yellow" href="#">
+        <h1 className="font-extrabold text-xl">HTML & CSS foundations</h1>
+      </a>
       <p className="text-base text-gray-500 font-medium	">
         These languages are the backbone of every website, defining structure,
         content, and presentation.
       </p>
-    </>
+    </div>
   );
 }
