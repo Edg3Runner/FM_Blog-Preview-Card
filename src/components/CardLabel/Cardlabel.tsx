03eaf33d5fc14fddfcbf68293e5f533a7c2fd0a5
Edg3Runner <mitch.pauwels@proton.me>
2024-04-25 15:02:22 +0100

    add border-radius on label

--- src/components/CardLabel/Cardlabel.tsx
+++ src/components/CardLabel/Cardlabel.tsx
@@ -1,6 +1,6 @@
 export default function Cardlabel() {
   return (
-    <span className="text-black font-bold bg-custom-yellow px-4 py-1 self-start	">
+    <span className="text-black font-bold bg-custom-yellow px-4 py-1 self-start rounded	">
       learning
     </span>
   );
